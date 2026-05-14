193398ca86b6b6dba8ce86b20989cc3673c2e74b
abdurohman-karim <ghostmagic766@gmail.com>
2026-05-14 17:19:26 +0500

    bugFix

--- routes/web.php
+++ routes/web.php
@@ -34,14 +34,14 @@ Route::get('/lang/{locale}', function (string $locale) use ($supportedWelcomeLoc
         ->cookie('welcome_locale', $locale, 60 * 24 * 365);
 })->name('welcome.language');
 
-Route::prefix('artisan-console')->name('artisan-console.')->middleware('throttle:20,1')->group(function () {
-    Route::get('/', [App\Http\Controllers\Blade\ArtisanConsoleController::class, 'index'])->name('index');
-    Route::post('/run', [App\Http\Controllers\Blade\ArtisanConsoleController::class, 'run'])->name('run')->middleware('throttle:10,1');
-});
-
 Auth::routes();
 Route::group(['middleware'=>"auth"],function (){
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+    Route::prefix('artisan-console')->name('artisan-console.')->middleware('throttle:20,1')->group(function () {
+        Route::get('/', [App\Http\Controllers\Blade\ArtisanConsoleController::class, 'index'])->name('index');
+        Route::post('/run', [App\Http\Controllers\Blade\ArtisanConsoleController::class, 'run'])->name('run')->middleware('throttle:10,1');
+    });
 
     Route::resource('users', App\Http\Controllers\Blade\UserController::class);
     Route::resource('roles', App\Http\Controllers\Blade\RolesController::class);
